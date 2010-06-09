dffb5dda328d98b050a8170e3d6c2c2fbb89c0b0
Tiziano Müller <dev-zero@gentoo.org>
2010-06-10 01:06:23 +1200

    Stop treating results from ldap_results as normal ldap return values
    Don't to treat results from ldap_results as normal ldap return
    values in 'sub error' ( as in 3.0.2 )

--- LDAPapi.pm
+++ LDAPapi.pm
@@ -1355,8 +1355,8 @@ sub result
     $self->{"result"} = $result;
     $self->{"status"} = $status;
 
-    $self->errorize($status);
     if( $status == -1 || $status == 0 ) {
+        $self->errorize($status);
         return undef;
     }
 
